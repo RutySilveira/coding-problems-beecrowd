a410629d497fae5d1eebbe8b291b5e8a08fba7e2
RutySilveira <ruteneles@gmail.com>
2025-06-04 22:53:47 -0300

    Adicionando resoluções do exercício 1007 em C++ e Python

---
+++ iniciante/1007/1007.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main()
+{
+  int a, b, c, d, diferenca;
+
+  std::cin >> a;
+  std::cin >> b;
+  std::cin >> c;
+  std::cin >> d;
+
+  diferenca = a * b - c * d;
+
+  std::cout << "DIFERENCA = " << diferenca << std::endl;
+
+  return 0;
+}
